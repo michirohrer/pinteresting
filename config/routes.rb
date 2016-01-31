58752d2553705927e01446589761a8e7218c74dc
Michael Rohrer <michael@on-line.at>
2016-01-31 15:51:18 +0100

    Creating home navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -23,7 +25,7 @@ Rails.application.routes.draw do
   #
   #     collection do
   #       get 'sold'
-  #     end
+  #     end  
   #   end
 
   # Example resource route with sub-resources:
